17222942c424b3ad8bdf7b152c305155b1cdccc5
Jeongyeon Park <ririsid@me.com>
2022-11-16 15:55:13 +0900

    Initial Commit

---
+++ CodingTest/CodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingTestApp.swift
+//  CodingTest
+//
+//  Created by 박정연 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct CodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodingTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CodingTest
+//
+//  Created by 박정연 on 2022/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
